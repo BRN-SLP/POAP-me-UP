12041cf191fc2f625dc3d23d8e193b60992de557
BoRn SliPPy <brn_slp@BoRns-MacBook-Pro.local>
2025-11-29 01:24:40 +0200

    Feat: Traced wave from approved design with smooth curves

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -17,54 +17,61 @@ export function Logo({ className, showText = true }: LogoProps) {
                 <svg
                     width="40"
                     height="40"
-                    viewBox="0 0 40 40"
+                    viewBox="0 0 100 100"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                     className="relative z-10"
                 >
                     <defs>
-                        <linearGradient id="logoGradient" x1="0" y1="40" x2="40" y2="0" gradientUnits="userSpaceOnUse">
+                        <linearGradient id="logoGradient" x1="0" y1="100" x2="100" y2="0" gradientUnits="userSpaceOnUse">
                             <stop offset="0%" stopColor="#0052FF" />
                             <stop offset="25%" stopColor="#7744DD" />
-                            <stop offset="50%" stopColor="#DD2255" />
-                            <stop offset="75%" stopColor="#FF7733" />
+                            <stop offset="50%" stopColor="#FF0420" />
+                            <stop offset="75%" stopColor="#FF8844" />
                             <stop offset="100%" stopColor="#FCFF52" />
                         </linearGradient>
                     </defs>
 
-                    {/* Wave forming S-shape based on approved design */}
+                    {/* Traced wave from approved design with smooth bottom-left */}
                     <path
-                        d="M 2 40
-                           L 2 30
-                           Q 2 22, 6 18
-                           Q 10 14, 16 12
-                           Q 22 10, 28 12
-                           Q 32 14, 34 18
-                           Q 36 22, 36 28
-                           Q 36 32, 33 35
-                           C 30 38, 26 38, 22 36
-                           Q 18 34, 16 30
-                           L 14 24
-                           Q 12 18, 10 14
-                           Q 8 10, 6 8
-                           Q 4 6, 4 4
-                           L 8 2
-                           Q 12 0, 18 2
-                           Q 24 4, 28 8
-                           Q 32 12, 34 18
-                           Q 36 24, 36 30
-                           Q 36 34, 34 36
-                           L 32 38
-                           Q 28 40, 22 40
-                           Q 16 40, 10 40
-                           Q 6 40, 4 40
+                        d="M 10 100
+                           C 10 100, 10 95, 10 88
+                           C 10 80, 12 72, 16 66
+                           C 20 60, 26 56, 34 54
+                           C 42 52, 50 52, 58 54
+                           C 66 56, 72 60, 76 66
+                           C 80 72, 82 80, 82 88
+                           C 82 94, 80 98, 76 100
+                           
+                           C 74 98, 70 94, 66 88
+                           C 62 82, 58 74, 54 66
+                           C 50 58, 46 50, 42 42
+                           C 38 34, 34 26, 30 20
+                           C 26 14, 22 10, 18 8
+                           C 14 6, 12 6, 12 8
+                           
+                           C 14 8, 18 10, 24 14
+                           C 30 18, 36 24, 42 32
+                           C 48 40, 54 48, 60 56
+                           C 66 64, 72 72, 78 78
+                           C 84 84, 88 88, 92 90
+                           C 96 92, 98 92, 98 90
+                           C 98 88, 96 84, 92 78
+                           C 88 72, 82 66, 76 60
+                           C 70 54, 64 50, 58 48
+                           C 52 46, 46 46, 40 48
+                           C 34 50, 28 54, 24 60
+                           C 20 66, 18 74, 18 82
+                           C 18 88, 18 92, 18 96
+                           C 18 98, 16 100, 14 100
+                           C 12 100, 10 100, 10 100
                            Z"
                         fill="url(#logoGradient)"
                         className="group-hover:scale-105 transition-transform duration-300 origin-center"
                     />
 
-                    {/* Wave crest at top-right */}
-                    <circle cx="37" cy="6" r="2.5" fill="#FCFF52" opacity="0.95"
+                    {/* Wave crest sparkle */}
+                    <circle cx="94" cy="16" r="5" fill="#FCFF52" opacity="0.95"
                         className="group-hover:opacity-100 group-hover:scale-110 transition-all duration-300" />
                 </svg>
             </div>
